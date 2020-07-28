c755fa9fe105d9346485353d3c0eae7e5757a2f8
PragmaticWays <adam.allard@live.com>
2020-07-27 21:01:15 -0400

    vid 22 exercise for flatList

--- app_setup_demo/src/screens/ListScreen.js
+++ app_setup_demo/src/screens/ListScreen.js
@@ -1,28 +1,49 @@
 import React from "react";
-import { Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const players = [
-    { id: "a", name: "Rodgers" },
-    { id: "b", name: "Adams" },
-    { id: "c", name: "Jones" },
-    { id: "d", name: "Funches" },
-    { id: "e", name: "Williams" },
+    { id: "a", name: "Rodgers", score: 95 },
+    { id: "b", name: "Adams", score: 98 },
+    { id: "c", name: "Jones", score: 97 },
+    { id: "d", name: "Funches", score: 76 },
+    { id: "e", name: "Williams", score: 83 },
   ];
   return (
     <FlatList
       keyExtractor={(player) => player.id}
       data={players}
       renderItem={({ item }) => {
-        return <Text>{item.name}</Text>;
+        return (
+          <View style={styles.container}>
+            <View style={styles.view}>
+              <Text style={styles.player}>Player {item.name}</Text>
+            </View>
+            <View style={styles.view}>
+              <Text style={styles.score}>Score {item.score}</Text>
+            </View>
+          </View>
+        );
       }}
     />
   );
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
+  container: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  view: {
+    flex: 1,
+  },
+  player: {
+    textAlign: "left",
+    fontSize: 20,
+  },
+  score: {
+    textAlign: "right",
+    fontSize: 20,
   },
 });
 
