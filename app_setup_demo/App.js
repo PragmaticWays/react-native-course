b4dae6948855c6256225fe500033f9f2722d0e2d
PragmaticWays <adam.allard@live.com>
2020-07-30 12:00:18 -0400

    reusage component with images

--- app_setup_demo/App.js
+++ app_setup_demo/App.js
@@ -1,9 +1,10 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentScreen from "./src/screens/ComponentScreen";
-import ListScreen from "./src/screens/ListScreen";
 import ImageScreen from "./src/screens/ImageScreen";
+import ListScreen from "./src/screens/ListScreen";
 
 const navigator = createStackNavigator(
   {
--- app_setup_demo/src/screens/ImageScreen.js
+++ app_setup_demo/src/screens/ImageScreen.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import ImageDetail from "../components/ImageDetail";
 
 const ImageScreen = () => {
-  return <Text>Image Screen</Text>;
+  const BEACH_IMG_SRC = "../../assets/images/beach.jpg";
+  const FOREST_IMG_SRC = "../../assets/images/forest.jpg";
+  const MOUNTAIN_IMG_SRC = "../../assets/images/mountain.jpg";
+
+  return (
+    <View>
+      <ImageDetail title="Beach" imageSource={require(BEACH_IMG_SRC)} />
+      <ImageDetail title="Forest" imageSource={require(FOREST_IMG_SRC)} />
+      <ImageDetail title="Mountain" imageSource={require(MOUNTAIN_IMG_SRC)} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
