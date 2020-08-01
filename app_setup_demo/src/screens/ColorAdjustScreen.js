38cea413b0a726c63604fcdd0b8562369304e3de
PragmaticWays <adam.allard@live.com>
2020-08-01 07:38:10 -0400

    refactoring color adjuster code

--- app_setup_demo/src/screens/ColorAdjustScreen.js
+++ app_setup_demo/src/screens/ColorAdjustScreen.js
@@ -9,25 +9,56 @@ const ColorAdjustScreen = () => {
 
   let colorStepSize = 10;
 
+  const setColor = (color, change) => {
+    const colorFactory = getColorFactory(color);
+    const RGB_MAX = 255;
+    const RGB_MIN = 0;
+    if (isWithinRgbRange(colorFactory.color, RGB_MIN, RGB_MAX)) {
+      let newColorValue = clamp(colorFactory.color + change, RGB_MIN, RGB_MAX);
+      colorFactory.setColor(newColorValue);
+    }
+  };
+
+  const getColorFactory = (color) => {
+    switch (color) {
+      case "red":
+        return { color: red, setColor: setRed };
+      case "green":
+        return { color: green, setColor: setGreen };
+      case "blue":
+        return { color: blue, setColor: setBlue };
+      default:
+        return {};
+    }
+  };
+
+  const isWithinRgbRange = (colorValue, RGB_MIN, RGB_MAX) => {
+    return colorValue >= RGB_MIN && colorValue <= RGB_MAX;
+  };
+
+  const clamp = (number, min, max) => {
+    return Math.min(Math.max(number, min), max);
+  };
+
   return (
     <View>
       <ColorAdjust
         colorName="Red"
         colorValue={red}
-        onIncrease={() => setRed(red + colorStepSize)}
-        onDecrease={() => setRed(red - colorStepSize)}
+        onIncrease={() => setColor("red", colorStepSize)}
+        onDecrease={() => setColor("red", -1 * colorStepSize)}
       />
       <ColorAdjust
         colorName="Green"
         colorValue={green}
-        onIncrease={() => setGreen(green + colorStepSize)}
-        onDecrease={() => setGreen(green - colorStepSize)}
+        onIncrease={() => setColor("green", colorStepSize)}
+        onDecrease={() => setColor("green", -1 * colorStepSize)}
       />
       <ColorAdjust
         colorName="Blue"
         colorValue={blue}
-        onIncrease={() => setBlue(blue + colorStepSize)}
-        onDecrease={() => setBlue(blue - colorStepSize)}
+        onIncrease={() => setColor("blue", colorStepSize)}
+        onDecrease={() => setColor("blue", -1 * colorStepSize)}
       />
       <View
         style={{
